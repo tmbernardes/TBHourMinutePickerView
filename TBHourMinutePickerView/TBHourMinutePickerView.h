9796ee43b7f526703788a176887f8bce0c59c45a
thiagotmb <tmb0710@gmail.com>
2016-01-26 15:24:22 -0200

    Initial Commit

---
+++ TBHourMinutePickerView/TBHourMinutePickerView.h
@@ -0,0 +1,19 @@
+//
+//  TBHourMinutePickerView.h
+//  TBHourMinutePickerView
+//
+//  Created by Thiago-Bernardes on 1/26/16.
+//  Copyright © 2016 TMB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TBHourMinutePickerView.
+FOUNDATION_EXPORT double TBHourMinutePickerViewVersionNumber;
+
+//! Project version string for TBHourMinutePickerView.
+FOUNDATION_EXPORT const unsigned char TBHourMinutePickerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TBHourMinutePickerView/PublicHeader.h>
+
+
